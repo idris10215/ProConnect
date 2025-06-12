ca9b0dacd4bb655af6b7e4849117d52d8ea87ab8
idris10215 <mohammedidris10215@gmail.com>
2025-06-12 10:49:01 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+const start = async () => {
+
+    const connectDB = await mongoose.connect(MONGO_URL);
+    if (connectDB) {
+        console.log('Connected to MongoDB');
+    } else {
+        console.error('Failed to connect to MongoDB');
+        process.exit(1);
+    }
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+start();
