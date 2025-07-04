93403f9f82fcf17fcc1893616681ab35a4bba1d1
idris10215 <mohammedidris10215@gmail.com>
2025-07-04 09:22:23 +0530

    Updated user routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -197,11 +197,11 @@ const convertuserDataToPDF = async (userData) => {
   doc.fontSize(16).text(`Email: ${userData.userId.email}`);
   doc.fontSize(16).text(`Username: ${userData.userId.username}`);
   doc.fontSize(16).text(`Bio: ${userData.bio || "No bio available"}`);
-  doc.fontSize(16).text(`Current position: ${userData.currentPosition || "No current position available"}`);
+  doc.fontSize(16).text(`Current position: ${userData.currentPost || "No current position available"}`);
 
   doc.fontSize(16).text("Past work")
   userData.pastWork.forEach((work, index) => {
-    doc.fontSize(14).text(`Company name: ${work.companyName}`);
+    doc.fontSize(14).text(`Company name: ${work.company}`);
     doc.fontSize(14).text(`Position: ${work.position}`);
     doc.fontSize(14).text(`Years: ${work.years}`);
   });
